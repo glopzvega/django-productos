c953f186ebd304b121895c9425e32e9232a4f3e9
GLVEGA <gerardolopez@uhipocrates.edu.mx>
2017-09-23 13:35:54 -0500

    HttpResponse Views

--- eshopApi/products/urls.py
+++ eshopApi/products/urls.py
@@ -4,5 +4,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	# Mostrar Todos ej. /products/
 	url(r'^$', views.index, name="index"),
+	# Mostrar Todos
+	url(r'^(?P<product_id>[0-9]+)/$', views.detail, name="detail"),
+	url(r'^(?P<product_id>[0-9]+)/ubicaciones/$', views.ubicaciones, name="ubicaciones"),
+	url(r'^(?P<product_id>[0-9]+)/votar/$', views.votar, name="votar"),
+	
 ]
--- eshopApi/products/views.py
+++ eshopApi/products/views.py
@@ -1,10 +1,38 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
 
+from .models import Product
+
+# Metodo usando shortcut render() no requiere loader ni HttpResponse
 def index(request):
-	return HttpResponse("Hola Mundo")
+	lista_productos = Product.objects.all()
+	context = {
+		"products" : lista_productos
+	}
+	return render(request, "products/index.html", context)
+
+# def index(request):
+# 	lista_productos = Product.objects.all()
+# 	template = loader.get_template("products/index.html")
+# 	context = {
+# 		"products" : lista_productos
+# 	}
+# 	return HttpResponse(template.render(context, request))
+
+def detail(request, product_id):	
+	product = "El producto seleccionado es %s."
+	return HttpResponse(product % product_id)
+
+def ubicaciones(request, product_id):
+	product = "Estas buscando las ubicaciones del producto %s."
+	return HttpResponse(product % product_id)
+
+def votar(request, product_id):
+	product = "Estas votando por el producto %s."
+	return HttpResponse(product % product_id)
